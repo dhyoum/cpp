182dae387981659dc83f6b958b0521e229ef8fba
dhyoum <dhyoum@gmail.com>
2019-07-16 17:54:39 +0900

    Update xtuple2.cc

--- src/xtuple2.cc
+++ src/xtuple2.cc
@@ -17,6 +17,40 @@ struct xtuple<T, Ts...> : xtuple<Ts...> {   // 어떻게 여기다 이렇게??
     static constexpr int N = BaseType::N+1;
 };
 
+template<int N, typename T>
+struct xtuple_element_type {
+	typedef T type;
+};
+template<typename T, typename ... Types>
+struct xtuple_element_type<0, xtuple<T, Types...>> {
+	typedef T type; 
+	typedef xtuple<T, Types...> tupleType; 
+};
+
+template<int N, typename T, typename ... Types>
+struct xtuple_element_type<N, xtuple<T, Types...>> {
+	typedef typename xtuple_element_type<N - 1, xtuple<Types...>>::type      type;
+	typedef typename xtuple_element_type<N - 1, xtuple<Types...>>::tupleType tupleType;
+};
+
+//--------------------------------------------------------------
+template<typename T> void foo(T& tp)
+{
+	typename xtuple_element_type<2, T>::type n;      // 1번째 요소 타입
+	typename xtuple_element_type<2, T>::tupleType t; // 1번째 요소를 저장하는 부모타입
+
+	cout << typeid(n).name() << endl;
+	cout << typeid(t).name() << endl;
+}
+
+template<int N, typename T> 
+typename xtuple_element_type<N, T>::type&   xget(T& tp)
+{
+//	return ((typename xtuple_element_type<N, T>::tupleType&)tp).value;
+
+	return (static_cast<typename xtuple_element_type<N, T>::tupleType&>(tp)).value;
+}
+
 int main()
 {
     xtuple<> t0;
